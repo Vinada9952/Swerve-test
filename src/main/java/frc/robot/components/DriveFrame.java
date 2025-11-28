5d84767f92aa4ee2317cacaa6518fc367be52f6e
Vincent <vincenttueminh@gmail.com>
2025-11-27 20:58:33 -0500

    Smart Dashboard display for swerve data (to test)

--- src/main/java/frc/robot/components/DriveFrame.java
+++ src/main/java/frc/robot/components/DriveFrame.java
@@ -18,6 +18,10 @@ import com.revrobotics.spark.SparkLowLevel;
 
 
 public class DriveFrame {
+    double x_speed; // Forward/backward
+    double y_speed; // Left/right
+    double rot_speed; // Rotation
+    
     Translation2d front_left_motor_position;
     Translation2d front_right_motor_position;
     Translation2d back_left_motor_position;
@@ -37,23 +41,38 @@ public class DriveFrame {
         back_left_motor_position = new Translation2d(-0.5, 0.5); // 0.5 back 0.5 left
         back_right_motor_position = new Translation2d(-0.5, -0.5); // 0.5 back 0.5 right
 
-        ChassisSpeeds speeds;
-
         kinematic_drive = new SwerveDriveKinematics(
             front_left_motor_position,
             front_right_motor_position,
             back_left_motor_position,
             back_right_motor_position
         );
+
+        SmartDashboard.putData("Speeds", builder -> {
+            builder.addDoubleProperty("Speed X", () -> getXSpeed(), null);
+            builder.addDoubleProperty("Speed Y", () -> getYSpeed(), null);
+            builder.addDoubleProperty("Speed Rot", () -> getRotSpeed(), null);
+        });
+
+        SmartDashboard.putData("Module States", builder -> {
+            builder.addDoubleProperty("Front Left Angle", () -> front_left_module_state.angle.getDegrees(), null);
+            builder.addDoubleProperty("Front Left Speed", () -> front_left_module_state.speedMetersPerSecond, null);
+            builder.addDoubleProperty("Front Right Angle", () -> front_right_module_state.angle.getDegrees(), null);
+            builder.addDoubleProperty("Front Right Speed", () -> front_right_module_state.speedMetersPerSecond, null);
+            builder.addDoubleProperty("Back Left Angle", () -> back_left_module_state.angle.getDegrees(), null);
+            builder.addDoubleProperty("Back Left Speed", () -> back_left_module_state.speedMetersPerSecond, null);
+            builder.addDoubleProperty("Back Right Angle", () -> back_right_module_state.angle.getDegrees(), null);
+            builder.addDoubleProperty("Back Right Speed", () -> back_right_module_state.speedMetersPerSecond, null);
+        });
     }
 
-    void drive( StickPosition left_stick, StickPosition right_stick ) {
-        double x_speed = left_stick.y(); // Forward/backward
-        double y_speed = left_stick.x(); // Left/right
-        double rot_speed = right_stick.x(); // Rotation
+
+    public void drive( StickPosition left_stick, StickPosition right_stick ) {
+        x_speed = left_stick.y(); // Forward/backward
+        y_speed = left_stick.x(); // Left/right
+        rot_speed = right_stick.x(); // Rotation
 
         ChassisSpeeds speeds = new ChassisSpeeds(x_speed, y_speed, rot_speed);
-        // TODO: display speeds on dashboard
         SwerveModuleState[] module_states = kinematic_drive.toSwerveModuleStates(speeds);
 
         front_left_module_state = module_states[0];
@@ -61,5 +80,17 @@ public class DriveFrame {
         back_left_module_state = module_states[2];
         back_right_module_state = module_states[3];
         // TODO: display modules states on dashboard
+    }
+
+    private double getXSpeed() {
+        return x_speed;
+    }
+
+    private double getYSpeed() {
+        return y_speed;
+    }
+
+    private double getRotSpeed() {
+        return rot_speed;
     }
 }
